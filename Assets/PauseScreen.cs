1c3048663496acedca5beb7a268fdc53836f0df9
Marlon <marlontdhuynh@gmail.com>
2026-04-06 01:39:14 -0700

    Implemented pause screen

---
+++ Assets/PauseScreen.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+using Unity.Collections; 
+
+public class PauseScreen : MonoBehaviour
+{
+    public bool isPaused; 
+    public GameObject pauseMenu; 
+
+    void Start()
+    {
+        isPaused = false; 
+    }
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape)) 
+        {
+            if (!isPaused){
+                pauseMenu.SetActive(true); 
+                isPaused = true; 
+                Time.timeScale = 0f;
+            }
+            else if (isPaused)
+            {
+                pauseMenu.SetActive(false);
+                isPaused = false;  
+                Time.timeScale = 1f;
+            }
+        }
+        
+    }
+}
